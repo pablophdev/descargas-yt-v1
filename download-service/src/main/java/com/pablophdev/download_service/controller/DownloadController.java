688988bf61e3070b852c2991be47d78a0b4e20c3
PedroPhilimon <pe.philimon@duocuc.cl>
2026-06-13 13:09:43 -0400

    Se modifica el controlador

--- download-service/src/main/java/com/pablophdev/download_service/controller/DownloadController.java
+++ download-service/src/main/java/com/pablophdev/download_service/controller/DownloadController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = {
     "http://127.0.0.1:5500", 
     "http://localhost:5500", 
-    "https://descargas-yt-v1-gtld-git-main-pablophdev-s-projects.vercel.app" // <-- Pegada aquí
+    "https://descargas-yt-v1-gtld-git-main-pablophdev-s-projects.vercel.app" // <-- Tu URL de Vercel
 }, exposedHeaders = "*")
 public class DownloadController {
 
